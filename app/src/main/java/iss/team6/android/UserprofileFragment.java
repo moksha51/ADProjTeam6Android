17bc1e0fdbcf73b6c82394ab3e4de64613c4f100
Nina Lim <e0008256@u.nus.edu>
2022-08-02 01:07:17 +0800

    layout - login,user profile, dashboard

---
+++ app/src/main/java/iss/team6/android/UserprofileFragment.java
@@ -0,0 +1,14 @@
+package iss.team6.android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserprofileFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_userprofile_fragment);
+    }
+}
