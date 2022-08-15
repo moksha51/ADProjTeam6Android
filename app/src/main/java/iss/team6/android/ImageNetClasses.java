e2c89ab172dd7f5cfe216388491f36fbafc47ba3
xingjietan <xingjie.tan@gmail.com>
2022-08-15 12:04:43 +0800

    Included Heily's portion

---
+++ app/src/main/java/iss/team6/android/ImageNetClasses.java
@@ -0,0 +1,12 @@
+package iss.team6.android;
+
+public class ImageNetClasses {
+    public static String[] IMAGENET_CLASSES = new String[]{
+            "cardboard",
+            "glass",
+            "metal",
+            "paper",
+            "plastic",
+            "trash",
+    };
+}
